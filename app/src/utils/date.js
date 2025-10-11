730914b55cc39a117ad44877b79d5416ab5bc04e
TyeNTy <mathias.coussat@free.fr>
2025-10-11 18:03:23 +0200

    Comments

--- app/src/utils/date.js
+++ app/src/utils/date.js
@@ -3,18 +3,23 @@ const timeUntilNow = (date) => {
   const diff = now.getTime() - date.getTime();
 
   if (diff < 1000 * 60) {
+    // less than 1 minute
     const secs = Math.floor(diff / 1000);
     return `${secs}s ago`;
   } else if (diff < 1000 * 60 * 60) {
+    // less than 1 hour
     const mins = Math.floor(diff / (1000 * 60));
     return `${mins}m ago`;
   } else if (diff < 1000 * 60 * 60 * 24) {
+    // less than 1 day
     const hours = Math.floor(diff / (1000 * 60 * 60));
     return `${hours}h ago`;
   } else if (diff < 1000 * 60 * 60 * 24 * 30) {
+    // less than 1 month (approximate)
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     return `${days}d ago`;
   } else {
+    // more than a month, show in months (approximate)
     const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
     return `${months}mo ago`;
   }
